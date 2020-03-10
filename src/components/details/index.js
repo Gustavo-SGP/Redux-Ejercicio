5ebdb959128ea5e309cbdc9cabf2674e90defc5f
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-03-10 19:05:45 -0300

    Base del código

---
+++ src/components/details/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './page';
+
+const Details = () => {
+    return (
+        <>
+          <Page />  
+        </>
+    );
+};
+
+export default Details;
---
+++ src/components/results/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './page';
+
+const Results = () => {
+    return (
+        <>
+          <Page />  
+        </>
+    );
+};
+
+export default Results;
